d5768db303bd0309268a75f5a9006b7c85f01930
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-04-08 20:24:58 -0800

    add welcome page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
+import Welcome from "../components/welcome/Welcome";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>hello worasl</h1>
+      <Welcome />
     </div>
   );
 }
