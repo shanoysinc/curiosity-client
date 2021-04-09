7b000720c7d96f80e2f26e992813a5147d840029
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-04-08 20:23:40 -0800

    add config for sass variables

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
 module.exports = {
-	future: {
-		webpack5: true,
-	},
-	typescript: {
-		// !! WARN !!
-		// Dangerously allow production builds to successfully complete even if
-		// your project has type errors.
-		// !! WARN !!
-		ignoreBuildErrors: true,
-	},
+  future: {
+    webpack5: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    // eslint-disable-next-line quotes
+    prependData: '@import "main.scss";',
+  },
 };
