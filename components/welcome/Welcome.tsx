c334c46df73d24e4bc6a2fd447cc3f07b4849b41
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-04-10 15:23:12 -0800

    add useEffect to check if user is authenticated

--- components/welcome/Welcome.tsx
+++ components/welcome/Welcome.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import styles from "../../styles/welcome/welcome.module.scss";
 import Logo from "../../assets/icons/Logo";
 import Signin from "../signin/Signin";
 import Signup from "../signup/Signup";
-
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useAuth } from "../../context/auth";
+import { url } from "../../util/url";
 const Welcome = () => {
-  const [showSignin, setSignin] = React.useState(true);
+  const [showSignin, setSignin] = useState(true);
+  const { dispatch } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    async function verifyUser() {
+      const token = localStorage.getItem("x-credentials") || null;
+      if (token) {
+        const auth = await axios.get(`${url}/auth/verify-token`, {
+          headers: { authorization: `Bearer ${token}` },
+          withCredentials: true,
+        });
+
+        if (auth.data.isAuth) {
+          const payload = { token, userInitials: auth.data.userInitials };
+          dispatch({ type: "login", payload });
+
+          router.push("/news-feed");
+        }
+      }
+    }
+
+    verifyUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className={styles.container}>
       <div className={styles.logo__container}>
