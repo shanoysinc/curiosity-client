4b11c142a2542f95f34b0f6dc00915617a35740b
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-04-09 16:10:47 -0800

    add authprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../styles/globals.scss";
 import { AppProps } from "next/app";
-
+import { QueryClient, QueryClientProvider } from "react-query";
+import { AuthProvider } from "../context/auth";
+const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
